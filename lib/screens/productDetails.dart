b4234682a63553672481adaa8a45f046e1571471
HillaryKiprono <kipronohillary240@gmail.com>
2023-09-05 14:15:14 +0300

    productDetails

--- lib/screens/productDetails.dart
+++ lib/screens/productDetails.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import '../models/singleItemData.dart';
 
 class ItemDetailPage extends StatelessWidget {
@@ -12,6 +11,64 @@ class ItemDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(item.title), // Display item title in the app bar
+      ),
+
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Container(
+          height: 80,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 50,
+                  child: Column(
+                    children: [
+                      const Text("Total Price"),
+                      Text(item.price,style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize: 20),)
+                    ],
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomLeft: Radius.circular(20)),
+                    color: Colors.green[500],
+                  ),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("1",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
+
+                    ],
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: Container(
+                  height: 50,
+                 decoration: BoxDecoration(
+                   borderRadius: BorderRadius.only(topRight: Radius.circular(20),bottomRight: Radius.circular(20)),
+                   color: Colors.black,
+                 ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Buy Now",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
+
+                    ],
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -32,19 +89,26 @@ class ItemDetailPage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16.0),
-            Text(
-              'Rating: ${item.rating}',
-              style: const TextStyle(
-                fontSize: 16.0,
-              ),
+            Row(
+              children: [
+                Icon(Icons.star,color: Colors.red,),
+                Text(
+                  item.rating,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  item.price,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 16.0),
-            Text(
-              'Price: ${item.price}',
-              style: const TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
+
+
           ],
         ),
       ),
