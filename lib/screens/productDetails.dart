a83c7f9206434bdbc9e992810a0179d8e2839e8c
HillaryKiprono <kipronohillary240@gmail.com>
2023-09-05 14:25:34 +0300

    productDetails

--- lib/screens/productDetails.dart
+++ lib/screens/productDetails.dart
@@ -10,9 +10,37 @@ class ItemDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(item.title), // Display item title in the app bar
+       elevation: 0.2,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.share_outlined,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.wallet,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Container(
@@ -26,90 +54,112 @@ class ItemDetailPage extends StatelessWidget {
                   child: Column(
                     children: [
                       const Text("Total Price"),
-                      Text(item.price,style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize: 20),)
+                      Text(
+                        item.price,
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      )
                     ],
                   ),
                 ),
               ),
-
               Expanded(
                 child: Container(
                   height: 50,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomLeft: Radius.circular(20)),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20)),
                     color: Colors.green[500],
                   ),
                   child: const Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("1",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
-
+                      Text(
+                        "1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
                     ],
                   ),
                 ),
               ),
-
               Expanded(
                 child: Container(
                   height: 50,
-                 decoration: BoxDecoration(
-                   borderRadius: BorderRadius.only(topRight: Radius.circular(20),bottomRight: Radius.circular(20)),
-                   color: Colors.black,
-                 ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20)),
+                    color: Colors.black,
+                  ),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Buy Now",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
-
+                      Text(
+                        "Buy Now",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
                     ],
                   ),
                 ),
               ),
-
             ],
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              item.imagePath,
-              width: double.infinity,
-              // height: 200.0,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 16.0),
-            Text(
-              item.description,
-              style: const TextStyle(
-                fontSize: 16.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                item.imagePath,
+                width: double.infinity,
+                // height: 200.0,
+                fit: BoxFit.cover,
               ),
-            ),
-            const SizedBox(height: 16.0),
-            Row(
-              children: [
-                Icon(Icons.star,color: Colors.red,),
-                Text(
-                  item.rating,
-                  style: const TextStyle(
-                    fontSize: 16.0,
-                  ),
+              const SizedBox(height: 16.0),
+              Text(
+                item.description,
+                style: const TextStyle(
+                  fontSize: 16.0,
                 ),
-                SizedBox(width: 10,),
-                Text(
-                  item.price,
-                  style: const TextStyle(
-                    fontSize: 16.0,
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.red,
                   ),
-                ),
-              ],
-            ),
-
-
-          ],
+                  Text(
+                    item.rating,
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    item.price,
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
